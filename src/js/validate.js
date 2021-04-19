2b6f6c544d23221af6282f5187078a22ad4af035
jing <jhu8187@gmail.com>
2021-04-19 20:46:18 +0800

    update validation

--- src/js/validate.js
+++ src/js/validate.js
@@ -8,13 +8,13 @@ function validate(event) {
   let valid = true;
   let lat = document.getElementById("latitude");
   let long = document.getElementById("longitude");
-  if(parseFloat(lat) > 90 || parseFloat(lat < -90)) {
+  if(parseFloat(lat) > 90 || parseFloat(lat) < -90) {
     let lat_error = document.getElementById("lat_error");
     lat_error.innerHTML = " must be a valid Latitude (-90 to 90)";
     valid = false;
   }
 
-  if(parseFloat(long > 180) || parseFloat(long < -180)) {
+  if(parseFloat(long) > 180 || parseFloat(long) < -180) {
     let long_error = document.getElementById("long_error");
     long_error.innerHTML = " must be a valid Longitude (-180 to 180)";
     valid = false;
